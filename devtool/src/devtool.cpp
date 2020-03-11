d2f2dae88d03a8a830daaf0d863f40df7def7a06
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2020-03-11 01:52:10 +0100

    Almost working CMake setup

--- devtool/src/devtool.cpp
+++ devtool/src/devtool.cpp
@@ -1,8 +1,12 @@
 #include <windows.h>
+#include <dnkvw/dnkvw.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(NULL, L"Devtool geladen", L"DaNiKhan V-Window Devtool", 0);
+
+    wchar_t buffer[256];
+    wsprintfW(buffer, L"%d", addNums(10, 7));
+    MessageBoxW(NULL, buffer, L"DaNiKhan V-Window Devtool", 0);
 
     return 0;
 }

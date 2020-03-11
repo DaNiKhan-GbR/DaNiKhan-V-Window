2de5b21a7a0ee0c55b303582611c9812dfa5f4f0
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2020-03-11 03:55:33 +0100

    OpenCV config
    Co-authored-by: Daniel Klintworth <daniel.klintworth@gmail.com>

--- devtool/src/devtool.cpp
+++ devtool/src/devtool.cpp
@@ -1,12 +1,77 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
 #include <windows.h>
 #include <dnkvw/dnkvw.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-    wchar_t buffer[256];
-    wsprintfW(buffer, L"%d", addNums(10, 7));
-    MessageBoxW(NULL, buffer, L"DaNiKhan V-Window Devtool", 0);
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    const wchar_t szWindowClass[] = L"DNKVW Devtool Mainwin";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = szWindowClass;
+    wc.hIcon = LoadIconW(NULL, IDI_INFORMATION);
+
+    RegisterClassW(&wc);
+
+    HWND hwnd = CreateWindowExW(
+        0,
+        szWindowClass,
+        L"DaNiKhan V-Window Devtool",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, // Parent
+        NULL, // Menu
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL)
+    {
+        return -1;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
 
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hwnd, &ps);
+
+                FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+                wchar_t buffer[256] = {0};
+                wsprintfW(buffer, L"%d", addNums(3, 4));
+                TextOutW(hdc, 20, 20, buffer, 20);
+
+                EndPaint(hwnd, &ps);
+            }
+            return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
